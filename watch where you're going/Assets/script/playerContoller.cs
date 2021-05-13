a3023ce912a8b7449dda01a3ba0c3d2074164814
doris624 <doris5730@gmail.com>
2021-05-13 23:49:20 +0800

    game

---
+++ watch where you're going/Assets/script/playerContoller.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerContoller : MonoBehaviour
+{
+
+    private Rigidbody playerRb;
+    public float speed = 5.0f;
+    private GameObject focalPoint;
+    public bool hasPowerUp = false;
+    private float powerupStrength = 15.0f;
+    public GameObject powerupIndicator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("focal point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput );
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("powerup"))
+        {
+            hasPowerUp = true;
+            powerupIndicator.gameObject.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(4);
+        hasPowerUp = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("enemy") && hasPowerUp)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+            Debug.Log("collided with" + collision.gameObject + "with powerup set to " + hasPowerUp);
+        }
+    }
+}
